46a179dd1d696f56925bd7bd3d0f162e17ed72e1
maharjun <maharjun@thoughtworks.com>
2017-07-30 08:26:33 +0530

    Added code for different paths in server

---
+++ src/html/MyHandler.java
@@ -0,0 +1,25 @@
+package html;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+
+class MyHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        File file = new File("./src/html/maharjun.html");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        String totalFile = "";
+        while((line = bufferedReader.readLine()) != null){
+            totalFile += line;
+        }
+        httpExchange.sendResponseHeaders(200, totalFile.length());
+        OutputStream outputStream = httpExchange.getResponseBody();
+        outputStream.write(totalFile.getBytes());
+        outputStream.close();
+    }
+}
