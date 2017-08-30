d02697d0c46558b2b140c471ec50852df40ba45c
maharjun <maharjun@thoughtworks.com>
2017-08-30 18:47:03 +0530

    Added Example code

--- src/html/Example.java
+++ src/html/Example.java
@@ -1,16 +1,16 @@
 package html;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) throws IOException {
-        File file = new File("./src/html/maharjun.html");
-        FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String line;
-        while((line = bufferedReader.readLine()) != null){
-            System.out.println(line);
-        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Double some = Double.parseDouble("31415926535897932384626433832795");
+        System.out.println(some);
+        System.out.println("1".compareTo("3"));
     }
 
 }
