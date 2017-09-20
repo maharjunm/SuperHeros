67ca6fceff94fc30cd0d11003ab42187828e60aa
maharjun <maharjun@thoughtworks.com>
2017-09-20 21:23:50 +0530

    it will find a pair which has maximum product of two numbers in a list

---
+++ src/amazon/MaxPair.java
@@ -0,0 +1,33 @@
+package amazon;
+
+import java.util.Scanner;
+
+public class MaxPair {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+        for (int i = 0; i < number; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int[] pair = findPair(array);
+        System.out.println(pair[0]);
+        System.out.println(pair[1]);
+    }
+
+    private static int[] findPair(int[] array) {
+        int first = array[0];
+        int second = array[1];
+        int maxNumber = second * first;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                int multiply = array[i] * array[j];
+                if (maxNumber >= maxNumber) {
+                    first = array[i];
+                    second = array[j];
+                }
+            }
+        }
+        return new int[]{first, second};
+    }
+}
