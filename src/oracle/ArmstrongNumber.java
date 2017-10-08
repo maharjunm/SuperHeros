afc8eb4fd74787ad73cc1cae86296a729d8a9b77
maharjun <maharjun@thoughtworks.com>
2017-10-08 15:17:23 +0530

    Amstrong number or not

---
+++ src/oracle/ArmstrongNumber.java
@@ -0,0 +1,26 @@
+package oracle;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println(isItArmstrongNumber(number));
+    }
+
+    private static boolean isItArmstrongNumber(int number) {
+        int sum = 0;
+        int dulicate = number;
+        while (number > 0) {
+            sum += Math.pow((int)(number % 10), 3);
+            number = number/10;
+        }
+        if(sum == dulicate)
+            return true;
+        return false;
+    }
+}
